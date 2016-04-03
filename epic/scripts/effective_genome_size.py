b24ac8beb86d8f48bc4fe2fc3b1377f6f52af02a
Endre Bakken Stovner <endrebak@idi.ntnu.no>
2016-04-03 21:13:04 +0200

    Multicore egs

--- epic/scripts/effective_genome_size.py
+++ epic/scripts/effective_genome_size.py
@@ -1,26 +1,49 @@
 from pyfaidx import Fasta
 from collections import defaultdict
 
+from joblib import Parallel, delayed
 
-def effective_genome_size(fasta, read_length=30):
+
+def effective_genome_size(fasta, read_length, nb_cores=1):
     """Compute effective genome size for genome."""
 
     idx = Fasta(fasta, read_ahead=int(10e5))
-    read_counts = defaultdict(int)
 
-    genome_length = 0
+    genome_length = sum([len(c) for c in idx])
 
-    for chromosome in idx:
+    results = Parallel(n_jobs=nb_cores)(
+        delayed(compute_number_effective_chromosome_reads)(
+            chromosome, read_length) for chromosome in idx)
 
-        genome_length += len(chromosome)
+    read_counts = sum([r[0] for r in results])
+    total_reads = sum([r[1] for r in results])
 
-        for start in range(0, len(chromosome) - read_length):
-            sequence = str(chromosome[start:start + read_length]).upper()
-
-            if not "N" in sequence:
-                read_counts[sequence] += 1
-
-    effective_genome_size = len(read_counts) / sum(read_counts.values())
-    effective_genome_size_with_n = len(read_counts) / genome_length
+    effective_genome_size = read_counts / total_reads
+    effective_genome_size_with_n = read_counts / genome_length
 
     return effective_genome_size, effective_genome_size_with_n
+
+
+def compute_number_effective_chromosome_reads(chromosome, read_length):
+
+    read_counts = defaultdict(int)
+
+    for start in range(0, len(chromosome) - read_length):
+        sequence = str(chromosome[start:start + read_length]).upper()
+
+        if not "N" in sequence:
+            read_counts[sequence] += 1
+
+    return len(read_counts), sum(read_counts.values())
+
+# def chromosome_chunks(chromosome, chunk_length, read_length):
+#     """Split chromosome into chunks that overlap by read_length."""
+
+#     for i in range(0, chunk_length):
+#         yield str(chromosome[i:i + chunk_length - read_length])
+
+#     for i in range(chunk_length, len(chromosome) - 1):
+#         yield str(chromosome[i:i + chunk_length - read_length])
+
+# for i in range(len(chromosome) - read_length, len(chromosome)):
+#     yield str(chromosome[i:i + chunk_length])
--- tests/scripts/test_effective_genome_size.py
+++ tests/scripts/test_effective_genome_size.py
@@ -3,9 +3,11 @@ import pytest
 import pandas as pd
 import numpy as np
 
+from pyfaidx import Fasta
+
 from io import StringIO
 
-from epic.scripts.effective_genome_size import effective_genome_size
+from epic.scripts.effective_genome_size import (effective_genome_size)
 
 
 @pytest.fixture
@@ -32,11 +34,6 @@ NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"""
     return str(f)
 
 
-@pytest.fixture
-def expected_result():
-    pass
-
-
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
@@ -44,8 +41,104 @@ def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
 @pytest.mark.scripts
 def test_effective_genome_size(fasta):
 
-    result, result_with_n = effective_genome_size(fasta)
+    result, result_with_n = effective_genome_size(fasta, read_length=30)
     print(result)
     print(result_with_n)
     assert 0
     # assert isclose(0.9548133595284872, result)
+
+
+"""Not bothering to implement chromosome chunks yet."""
+
+# @pytest.fixture
+# def expected_result_chromosome_chunks():
+#     return ["TA", "AC", "CG", "GC", "CC", "CC", "CC"]
+
+# @pytest.fixture
+# def fasta_simple(tmpdir):
+
+#     f = tmpdir.join("fasta.simple")
+#     contents = """>chr1
+# TACG
+# CCCC"""
+
+#     f.write(contents)
+
+#     return Fasta(str(f))
+
+# @pytest.fixture
+# def fasta_pyfaidx(fasta, expected_result_chromosome_chunks):
+
+#     return Fasta(fasta, read_ahead=int(10e3))
+
+# @pytest.mark.scripts
+# def test_chromosome_chunks(fasta_simple, expected_result_chromosome_chunks):
+
+#     chromosome = fasta_simple["chr1"]
+
+#     result = list(chromosome_chunks(chromosome, 4, 2))
+
+#     print(result)
+
+#     # assert 0
+#     assert result == expected_result_chromosome_chunks
+
+# @pytest.fixture
+# def fasta_simple2(tmpdir):
+
+#     f = tmpdir.join("fasta.simple2")
+#     contents = """>chr1
+# TACG
+# CCCC
+# GGG"""
+
+#     f.write(contents)
+
+#     return Fasta(str(f))
+
+# @pytest.fixture
+# def expected_result_chromosome_chunks2():
+#     return ["TA", "AC", "CG", "GC", "CC", "CC", "CC", "CG", "GG", "GG"]
+
+# @pytest.mark.scripts
+# def test_chromosome_chunks2(fasta_simple2, expected_result_chromosome_chunks2):
+
+#     chromosome = fasta_simple2["chr1"]
+
+#     result = list(chromosome_chunks(chromosome, 4, 2))
+
+#     print(result)
+
+#     assert result == expected_result_chromosome_chunks2
+
+# @pytest.fixture
+# def fasta_simple3(tmpdir):
+
+#     f = tmpdir.join("fasta.simple3")
+#     contents = """>chr1
+# TACG
+# CCCC
+# GGG"""
+
+#     f.write(contents)
+
+#     return Fasta(str(f))
+
+# # @pytest.fixture
+# # def expected_result_chromosome_chunks3():
+# #     return ["TA", "AC", "CG", "GC", "CC", "CC", "CC", "CG", "GG", "GG"]
+
+# # @pytest.mark.scripts
+# # def test_chromosome_chunks3(fasta_simple3, expected_result_chromosome_chunks3):
+
+# #     chromosome = fasta_simple3["chr1"]
+
+# #     result = list(chromosome_chunks(chromosome, 3, 2))
+
+# #     print(result)
+
+# #     assert result == expected_result_chromosome_chunks3
+
+# # @pytest.fixture
+# # def expected_result():
+# #     pass
