117e9758dc38d5603ab141f19e54cd2451ccb1a2
Endre Bakken Stovner <endrebak@stud.ntnu.no>
2016-05-10 19:31:09 +0200

    commit so can checkout others

--- tests/config/test_genomes.py
+++ tests/config/test_genomes.py
@@ -15,13 +15,11 @@ def input_data():
 
 @pytest.fixture
 def expected_result():
-    pass
+    return {'chr8': 146364022, 'chr12': 133851895, 'chr9': 141213431, 'chr21': 48129895, 'chr15': 102531392, 'chr14': 107349540, 'chrY': 59373566, 'chr10': 135534747, 'chr17': 81195210, 'chrX': 155270560, 'chr11': 135006516, 'chr18': 78077248, 'chr2': 243199373, 'chr6': 171115067, 'chr3': 198022430, 'chr20': 63025520, 'chr22': 51304566, 'chr13': 115169878, 'chr19': 59128983, 'chr5': 180915260, 'chrM': 16571, 'chr7': 159138663, 'chr16': 90354753, 'chr4': 191154276, 'chr1': 249250621} 
 
 
 @pytest.mark.unit
 def test_create_genome_size_dict(input_data, expected_result):
 
     result = create_genome_size_dict("hg19")
-    print(result)
-    assert 0
-    # assert result == expected_result
+    assert result == expected_result
