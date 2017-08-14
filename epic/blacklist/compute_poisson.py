f8cd2c68f4d67cb1bf5dea0d642bde15434c2e35
endre bakken stovner <endrebakkenstovner@ans-180249.stolav.ntnu.no>
2017-08-14 13:33:56 +0200

    Add epic-blacklist script

---
+++ epic/blacklist/compute_poisson.py
@@ -0,0 +1,35 @@
+from scipy.stats.distributions import poisson
+from statsmodels.sandbox.stats.multicomp import multipletests
+
+import pandas as pd
+
+def compute_poisson(df, args):
+
+    bins = int(args.effective_genome_fraction/int(args.bin_size))
+
+    bad_bins = []
+    for s in df:
+        s = df[s]
+        unique_alignments = s.sum()
+
+        average = int(unique_alignments)/bins
+
+        value_counts = s.value_counts().fillna(0).index.get_level_values(0)
+        poisson_scores = pd.Series(poisson.sf(value_counts, mu=average))
+        poisson_scores = pd.concat([pd.Series(value_counts).to_frame(), poisson_scores], axis=1)
+        poisson_scores.columns = ["Value", "Score"]
+        poisson_scores = poisson_scores.set_index("Value")
+
+        poisson_scores = pd.Series(index=poisson_scores.index, data=poisson_scores.Score)
+
+        poisson_p_vals = s.replace(poisson_scores.to_dict())
+
+        fdr = multipletests(poisson_p_vals, method="fdr_bh")[1]
+        fdr = pd.Series(fdr, index=s.index, name="fdr")
+
+        fdr_df = pd.concat([s, fdr], axis=1)
+
+        r = fdr_df[fdr_df.fdr < args.fdr]
+        bad_bins.append(r)
+
+    return pd.concat(bad_bins, axis=1).index.to_frame().reset_index(drop=True)
---
+++ tests/blacklist/test_blacklist.py
@@ -0,0 +1,55 @@
+import pytest
+
+from io import StringIO
+from collections import namedtuple
+
+import pandas as pd
+
+from epic.blacklist.compute_poisson import compute_poisson
+
+@pytest.fixture()
+def matrix():
+
+    return pd.read_table(StringIO("""Chromosome Bin f1.bed f2.bed
+chr1 0 1.0 1.0
+chr1 200 1.0 1.0
+chr1 400 1.0 10.0
+chr1 600 1.0 1.0
+chr1 800 10.0 500.0
+chr1 1000 1.0 2.0
+chr1 2000 1.0 2.0
+chr1 2200 1.0 600.0
+chr1 2400 1.0 2.0"""), sep=" ", header=0, index_col=[0, 1])
+
+
+MockArgs = namedtuple("MockNamespace",
+        ["number_cores", "genome", "keep_duplicates", "bin_size",
+         "fragment_size", "fdr", "effective_genome_fraction", "chromosome_sizes"])
+
+
+@pytest.fixture()
+def effective_genome_size_dict():
+    return {"chr1": 2000}
+
+
+@pytest.fixture()
+def mock_args():
+
+    return MockArgs(1, "hg38", True, 200, 150, 0.05, 2000, None)
+
+
+@pytest.fixture()
+def expected_result():
+    return pd.read_table(StringIO("""Bin Chromosome
+0   800       chr1
+1  2200       chr1"""), index_col=0, header=0, sep="\s+")
+
+
+def test_compute_poisson(matrix, mock_args, expected_result):
+
+    result = compute_poisson(matrix, mock_args)
+
+    print(result)
+    print(expected_result)
+
+    assert result.equals(expected_result)
